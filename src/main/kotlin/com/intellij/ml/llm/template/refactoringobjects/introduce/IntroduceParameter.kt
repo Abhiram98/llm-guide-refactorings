1a18de5a664bd9c793951d7dec954bf46f83c295
Abhiram98 <bellur.abhiram@gmail.com>
2025-03-25 15:58:15 -0600

    add introduce parameter impl

---
+++ src/main/kotlin/com/intellij/ml/llm/template/refactoringobjects/introduce/IntroduceParameter.kt
@@ -0,0 +1,68 @@
+package com.intellij.ml.llm.template.refactoringobjects.introduce
+
+import com.intellij.ml.llm.template.refactoringobjects.AbstractRefactoring
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiExpression
+import com.intellij.psi.PsiFile
+import com.intellij.psi.PsiLocalVariable
+import com.intellij.psi.PsiMethod
+import com.intellij.refactoring.introduceParameter.IntroduceParameterProcessor
+import com.intellij.refactoring.introduceVariable.IntroduceVariableBase
+import com.intellij.refactoring.suggested.startOffset
+import it.unimi.dsi.fastutil.ints.IntList
+import org.jetbrains.kotlin.psi.psiUtil.endOffset
+
+class IntroduceParameter(
+    override val startLoc: Int,
+    override val endLoc: Int,
+    val paramName: String,
+    val methosPsi: PsiMethod,
+    val expression: PsiExpression,
+    val localVariable: PsiLocalVariable?
+) : AbstractRefactoring() {
+    override fun isValid(project: Project, editor: Editor, file: PsiFile): Boolean {
+        return true
+    }
+
+    override fun getRefactoringPreview(): String {
+        return "introduce parameter $paramName from ${expression.text}"
+    }
+
+    override fun getStartOffset(): Int {
+        return expression.startOffset
+    }
+
+    override fun getEndOffset(): Int {
+        return expression.endOffset
+    }
+
+    override fun getReverseRefactoringObject(project: Project, editor: Editor, file: PsiFile): AbstractRefactoring? {
+        TODO("Not yet implemented")
+    }
+
+    override fun recalibrateRefactoring(project: Project, editor: Editor, file: PsiFile): AbstractRefactoring? {
+        TODO("Not yet implemented")
+    }
+
+    override fun performRefactoring(project: Project, editor: Editor, file: PsiFile) {
+        super.performRefactoring(project, editor, file)
+        IntroduceParameterProcessor(
+            project,
+            methosPsi,
+            methosPsi,
+            expression,
+            expression,
+            localVariable,
+            true,
+            paramName,
+            IntroduceVariableBase.JavaReplaceChoice.ALL,
+            1,
+            false,
+            false,
+            false,
+            expression.type,
+            IntList.of()
+        ).run()
+    }
+}
--- src/test/kotlin/com/intellij/ml/llm/template/refactoringobjects/introduceparam/IntroduceParamTest.kt
+++ src/test/kotlin/com/intellij/ml/llm/template/refactoringobjects/introduceparam/IntroduceParamTest.kt
@@ -1,8 +1,12 @@
 package com.intellij.ml.llm.template.refactoringobjects.introduceparam
 
+import com.intellij.ml.llm.template.refactoringobjects.introduce.IntroduceParameter
 import com.intellij.ml.llm.template.utils.PsiUtils
+import com.intellij.psi.PsiDeclarationStatement
 import com.intellij.psi.PsiLiteralExpression
+import com.intellij.psi.PsiLocalVariable
 import com.intellij.psi.PsiMethod
+import com.intellij.refactoring.actions.IntroduceFieldAction
 import com.intellij.refactoring.introduceParameter.IntroduceParameterProcessor
 import com.intellij.refactoring.introduceVariable.IntroduceVariableBase
 import com.intellij.testFramework.LightPlatformCodeInsightTestCase
@@ -16,36 +20,62 @@ class IntroduceParamTest: LightPlatformCodeInsightTestCase() {
 
     fun testIntroduceParam(){
         configureByFile("/testdata/HelloWorld.java")
-        val lineNumber = 14
+        val lineNumber = 13
         val psiMethods: List<PsiMethod> = PsiUtils.getElementsOfTypeOnLine(
             file, editor, lineNumber, PsiMethod::class.java
         )
         assert(psiMethods.isNotEmpty())
         val methodToReplace = psiMethods[0]
         val psiLiterals: List<PsiLiteralExpression> = PsiUtils.getElementsOfTypeOnLine(
-            file, editor, 17, PsiLiteralExpression::class.java
+            file, editor, 16, PsiLiteralExpression::class.java
         )
         assert(psiLiterals.isNotEmpty())
 
-
-        IntroduceParameterProcessor(
-            project,
-            methodToReplace,
-            methodToReplace,
-            psiLiterals[0],
-            psiLiterals[0],
-            null,
-            false,
-            "printString",
-            IntroduceVariableBase.JavaReplaceChoice.ALL,
-            1,
-            false,
-            false,
-            false,
-            psiLiterals[0].type,
-            IntList.of()
-        ).run()
+        val refObj = IntroduceParameter(13, 13, "printString", methodToReplace, psiLiterals[0], null)
+        refObj.performRefactoring(project, editor, file)
 
         println(file.text)
+
+        assert(file.text.contains("""    public void linearSearch(List<Integer> array, int value, java.lang.String printString){
+        for (int i=0;i<array.size();i++){
+            if (array.get(i) ==value)
+                System.out.println(printString);
+        }
+    }"""))
+
+    }
+
+    fun `test introduce param 2`(){
+        configureByFile("/testdata/HelloWorld.java")
+        val lineNumber = 51
+        val psiMethods: List<PsiMethod> = PsiUtils.getElementsOfTypeOnLine(
+            file, editor, lineNumber, PsiMethod::class.java
+        )
+        assert(psiMethods.isNotEmpty())
+        val methodToReplace = psiMethods[0]
+        val psiLiterals: List<PsiLiteralExpression> = PsiUtils.getElementsOfTypeOnLine(
+            file, editor, 52, PsiLiteralExpression::class.java
+        )
+        val psiLocalVariables: List<PsiLocalVariable> = PsiUtils.getElementsOfTypeOnLine(
+            file, editor, 52, PsiLocalVariable::class.java
+        )
+        assert(psiLiterals.isNotEmpty())
+
+        val refObj = IntroduceParameter(13, 13, "printString", methodToReplace, psiLiterals[0], psiLocalVariables[0])
+        refObj.performRefactoring(project, editor, file)
+
+        println(file.text)
+
+        assert(file.text.contains("""    public static void prettyPrintArray(List<Integer> array, java.lang.String printString){
+
+        for(int i = 0; i < array.size(); i++) {
+            printString += "Element: "+(i+1);
+            printString += array.get(i).toString();
+            printString += "\n";
+        }
+        System.out.println(printString);
+    }"""))
+
+
     }
 }
