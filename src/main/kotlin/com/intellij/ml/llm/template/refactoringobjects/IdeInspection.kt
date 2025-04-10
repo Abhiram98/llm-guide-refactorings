443d2ed4321198c25625416b900dd4ab8f98e5e1
Abhiram98 <bellur.abhiram@gmail.com>
2025-04-10 10:04:07 -0600

    filter for error severity

--- src/main/kotlin/com/intellij/ml/llm/template/refactoringobjects/IdeInspection.kt
+++ src/main/kotlin/com/intellij/ml/llm/template/refactoringobjects/IdeInspection.kt
@@ -1,10 +1,12 @@
 package com.intellij.ml.llm.template.refactoringobjects
 
 import com.intellij.analysis.AnalysisScope
+import com.intellij.codeHighlighting.HighlightDisplayLevel
 import com.intellij.codeInspection.ProblemDescriptorBase
-import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.codeInspection.actions.CodeInspectionAction
 import com.intellij.codeInspection.ex.GlobalInspectionContextImpl
+import com.intellij.codeInspection.ui.InspectionTreeNode
+import com.intellij.codeInspection.ui.ProblemDescriptionNode
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import com.intellij.ui.tree.TreePathUtil
@@ -25,30 +27,36 @@ class IdeInspection(val project: Project, val scope: AnalysisScope, val file: Ps
 //                        view.tree.root // traverse tree and find problems.
     }
     fun waitForCompletion(){
+        Thread.sleep(5000)
         if (myGlobalInspectionContext == null)
             return
         while(myGlobalInspectionContext!!.view == null){
             Thread.sleep(1000)
         }
-        val p = myGlobalInspectionContext!!.view.tree.getSelectedDescriptorPacks(
-            true, mutableSetOf(file.virtualFile),true, arrayOf(TreePathUtil.pathToTreeNode(myGlobalInspectionContext!!.view.tree.root))
-        )
-        val problemDescriptions = p.map { it.filter {
-                it2 ->
-            val problem = it2 as? ProblemDescriptorBase
-//            problem?.highlightType == ProblemHighlightType.ERROR || problem?.highlightType == ProblemHighlightType.GENERIC_ERROR
-            problem!=null
-        }.map {
-                it2 ->
-            val problem = it2 as? ProblemDescriptorBase
-            problem
-        }.filterNotNull()
-            .map {
-                    it2 -> "${it2.highlightType} on line ${it2.lineNumber}: ${it2.toString()}"
-            }
-        }.reduce { acc, strings -> acc + strings }
-        problems.addAll(problemDescriptions)
+
+        val root = myGlobalInspectionContext!!.view.tree.root
+        val errors = getAllProblemChildren(root)
+        val descriptions = errors.filter {
+            val level = it.javaClass.getDeclaredField("myLevel")
+            level.isAccessible = true
+            (level.get(it) as HighlightDisplayLevel) == HighlightDisplayLevel.ERROR
+        }.map{
+            val desc = (it.descriptor as? ProblemDescriptorBase)
+            "Error on line ${desc?.lineNumber}: ${it}"
+        }
+        problems.addAll(descriptions)
 
         println("got the view!")
+    }
+
+    private fun getAllProblemChildren(root: InspectionTreeNode): List<ProblemDescriptionNode>{
+        val problemNodes = mutableListOf<ProblemDescriptionNode>()
+        for (c in root.children){
+            val problemDescriptionNode = c as? ProblemDescriptionNode
+            if (problemDescriptionNode !=null)
+                problemNodes.add(problemDescriptionNode)
+            problemNodes.addAll(getAllProblemChildren(c))
+        }
+        return problemNodes
     }
 }
