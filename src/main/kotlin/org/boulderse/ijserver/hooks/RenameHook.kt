32e856f2167f78e7f888d929dc6244b09ce23307
Abhiram98 <bellur.abhiram@gmail.com>
2025-10-29 14:11:22 -0600

    capture the name

--- src/main/kotlin/org/boulderse/ijserver/hooks/RenameHook.kt
+++ src/main/kotlin/org/boulderse/ijserver/hooks/RenameHook.kt
@@ -1,44 +1,43 @@
 package org.boulderse.ijserver.hooks
 
 import com.intellij.notification.NotificationAction
-import com.intellij.notification.NotificationGroup
-import com.intellij.notification.NotificationGroupManager
 import com.intellij.notification.NotificationType
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.ProjectActivity
-import com.intellij.psi.PsiElement
 import com.intellij.refactoring.listeners.RefactoringEventData
 import com.intellij.refactoring.listeners.RefactoringEventListener
-import org.boulderse.ijserver.LLMBundle
 import org.boulderse.ijserver.createNotificationGroup
-import org.boulderse.ijserver.settings.openSettingsDialog
+import org.jetbrains.kotlin.asJava.namedUnwrappedElement
 
 class RenameHook : ProjectActivity {
     var coRenameInProgress = false
+    var seedOldName: String? = null
+    var seedNewName: String? = null
 
     fun registerHook(project: Project) {
         project.messageBus.connect().subscribe(
             RefactoringEventListener.REFACTORING_EVENT_TOPIC,
-            RefactoringEventListener { refactoringId, afterData ->
-                print("refactoring done: $refactoringId")
-                if ("rename" in refactoringId && !coRenameInProgress) {
-                    print("Found a rename refactoring")
-                    print(afterData)
-                    val notification =
-                        createNotificationGroup().createNotification(
-                            "Trigger coRenameAgent",
-                            "You triggered a rename. Do you want to trigger the CoRenameAgent.",
-                            NotificationType.INFORMATION,
-                        )
 
-                    val action = "Trigger agent"
-                    notification.addAction(
-                        NotificationAction.createSimple(action) {
-                            triggerAgent()
-                            notification.expire()
-                        },
-                    )
-                    notification.notify(project)
+            object : RefactoringEventListener {
+                override fun refactoringStarted(refactoringId: String, beforeData: RefactoringEventData?) {
+                    print("refactoring started: $refactoringId")
+                    super.refactoringStarted(refactoringId, beforeData)
+                    if ("rename" in refactoringId && !coRenameInProgress) {
+                        print("Found a rename refactoring")
+                        val element = beforeData?.getUserData(RefactoringEventData.PSI_ELEMENT_KEY)
+                        seedOldName = element?.namedUnwrappedElement?.name
+                    }
+                }
+
+                override fun refactoringDone(refactoringId: String, afterData: RefactoringEventData?) {
+                    print("refactoring done: $refactoringId")
+                    if ("rename" in refactoringId && !coRenameInProgress) {
+                        print("Found a rename refactoring")
+                        val element = afterData?.getUserData(RefactoringEventData.PSI_ELEMENT_KEY)
+                        seedNewName = element?.namedUnwrappedElement?.name
+                        showNotification(project)
+                    }
+
                 }
             },
         )
@@ -50,11 +49,31 @@ class RenameHook : ProjectActivity {
 
     fun triggerAgent() {
         coRenameInProgress = true
+        seedNewName!!
+        seedOldName!!
         // todo: trigger python agent.
         //  docker run cuboulderse/renameagent --args
     }
 
     fun agentComplete() {
         coRenameInProgress = false
+    }
+
+    fun showNotification(project: Project) {
+        val notification =
+            createNotificationGroup().createNotification(
+                "Trigger coRenameAgent",
+                "You triggered a rename. Do you want to trigger the CoRenameAgent.",
+                NotificationType.INFORMATION,
+            )
+
+        val action = "Trigger agent"
+        notification.addAction(
+            NotificationAction.createSimple(action) {
+                triggerAgent()
+                notification.expire()
+            },
+        )
+        notification.notify(project)
     }
 }
